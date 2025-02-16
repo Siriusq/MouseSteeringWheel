8d31d767b4e6a5737f42b5c8fd0d0e5e5039c77c
Siriusq <eargon.skyle@gmail.com>
2025-02-16 20:56:16 +0800

    Add Dead Zone

--- MouseSteeringWheel/Services/MouseInputService.cs
+++ MouseSteeringWheel/Services/MouseInputService.cs
@@ -21,8 +21,12 @@ namespace MouseSteeringWheel.Services
         // 设置屏幕中央为原点
         private int OriginX = Screen.PrimaryScreen.Bounds.Width / 2;
         private int OriginY = Screen.PrimaryScreen.Bounds.Height / 2;
-        private int sensitivityX = 60;
+        // 设置灵敏度
+        private int sensitivityX = 30;
         private int sensitivityY = 30;
+        // 设置死区百分比
+        private double deadZoneXPercentage = 0.1;
+        private double deadZoneYPercentage = 0.1;
 
         public void OnMouseMove(object sender, EventArgs e)
         {
@@ -44,11 +48,22 @@ namespace MouseSteeringWheel.Services
             int newJoystickX = 16383 + dx;
             int newJoystickY = 16383 + dy;
 
+            // 设置死区
+            int deadZoneX = (int)(16383 * deadZoneXPercentage);
+            int deadZoneY = (int)(16383 * deadZoneYPercentage);
+            int deadZoneLeft = 16383 - deadZoneX;
+            int deadZoneRight = 16383 + deadZoneX;
+            int deadZoneUp = 16383 - deadZoneY;
+            int deadZoneDown = 16383 + deadZoneY;
+
             // 设置阈值
-            if (newJoystickX < 0) newJoystickX = 0;
-            if (newJoystickX > 32767) newJoystickX = 32767;
-            if (newJoystickY < 0) newJoystickY = 0;
-            if (newJoystickY > 32767) newJoystickY = 32767;
+            if (newJoystickX > deadZoneLeft && newJoystickX < deadZoneRight) newJoystickX = 16383;
+            else if (newJoystickX < 0) newJoystickX = 0;
+            else if (newJoystickX > 32767) newJoystickX = 32767;
+
+            if (newJoystickY > deadZoneUp && newJoystickY < deadZoneDown) newJoystickY = 16383;
+            else if (newJoystickY < 0) newJoystickY = 0;
+            else if (newJoystickY > 32767) newJoystickY = 32767;
 
             // 更新摇杆XY位置
             _vJoyService.SetJoystickX(newJoystickX);
