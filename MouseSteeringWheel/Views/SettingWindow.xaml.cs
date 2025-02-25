9aec2a62d1769a37c4fb9b45df09405190bed8c0
Siriusq <eargon.skyle@gmail.com>
2025-02-25 21:47:48 +0800

    Add Language Switch Function

--- MouseSteeringWheel/Views/SettingWindow.xaml.cs
+++ MouseSteeringWheel/Views/SettingWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +25,8 @@ namespace MouseSteeringWheel.Views
         public SettingWindow()
         {
             InitializeComponent();
+            //设置语言单选框状态
+            SetLanguageRadioButton();
         }
 
         private void Pause_HotKeyTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -43,5 +48,52 @@ namespace MouseSteeringWheel.Views
         {
 
         }
+
+        #region 语言切换
+        private void English_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SetLanguage("en-US");
+        }
+
+        private void Chinese_Simplified_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SetLanguage("zh-CN");
+
+        }
+
+        private void Chinese_Traditional_RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SetLanguage("zh-Hant");
+        }
+
+        private void SetLanguage(string cultureName)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureName);
+        }
+
+        // 自动勾选当前语言对应的 RadioButton
+        private void SetLanguageRadioButton()
+        {
+            // 获取当前 UI 语言
+            string currentCulture = CultureInfo.CurrentUICulture.Name;
+
+            switch (currentCulture)
+            {
+                case "en-US":
+                    EnglishRadioButton.IsChecked = true;
+                    break;
+                case "zh-CN":
+                    ChineseSimplifiedButton.IsChecked = true;
+                    break;
+                case "zh-Hant":
+                    ChineseTraditionalButton.IsChecked = true;
+                    break;
+                default:
+                    EnglishRadioButton.IsChecked = true;
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
