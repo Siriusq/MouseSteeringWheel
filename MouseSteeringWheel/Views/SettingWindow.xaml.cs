d85126257d0e9c1c89e97ab968f602c51faacf34
Siriusq <eargon.skyle@gmail.com>
2025-02-27 17:26:21 +0800

    Binding HotKey Releated Textbox with PreviewKeyUp and PreviewKeyDown Event

--- MouseSteeringWheel/Views/SettingWindow.xaml.cs
+++ MouseSteeringWheel/Views/SettingWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using MouseSteeringWheel.Properties;
 using MouseSteeringWheel.Services;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace MouseSteeringWheel.Views
@@ -13,11 +16,13 @@ namespace MouseSteeringWheel.Views
     /// </summary>
     public partial class SettingWindow : Window
     {
+        private MessageBoxService _messageBoxService;
         public SettingWindow()
         {
             InitializeComponent();
             //读取设置并设置UI状态
             ReadSettings();
+            _messageBoxService = new MessageBoxService();
         }
 
         // 点击关闭和取消按钮时隐藏设置窗口
@@ -25,26 +30,6 @@ namespace MouseSteeringWheel.Views
         {
             this.Hide();
             e.Cancel = true;
-        }
-
-        private void Pause_HotKeyTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Setting_HotKeyTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Reset_Joystick_HotKeyTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Button_Binding_HotKeyTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
         }
 
         #region 语言切换
@@ -63,12 +48,113 @@ namespace MouseSteeringWheel.Views
         private void JoystickButtonChecked(object sender, RoutedEventArgs e) => _currUIStyle = 2;
         #endregion
 
-        #region X轴
+        #region 快捷键
+        private readonly HashSet<Key> _pressedKeys = new HashSet<Key>();
+        private string _lastKeyCombination;
+        private int _currKeyID = 0;
+        private Key _currHotKey;
+        private ModifierKeys _currHotKeyModifiers;
+        private Key[] _hotKeyArray = new Key[132];
+        private ModifierKeys[] _modifierKeyArray = new ModifierKeys[132];
+
+        // 快捷键预处理
+        private void HotKeyTextBoxPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //判断快捷键对应的文本框编号
+            var textBox = (TextBox)sender;
+            if (textBox.Name == "ButtonHotKeyTextBox")
+            {
+                //判断ID是否合法
+                try
+                {
+                    _currKeyID = int.Parse(vJoyButtonID.Text);
+                    if (_currKeyID <= 0 || _currKeyID > 128)
+                    {
+                        _messageBoxService.ShowMessage("vJoy 按键 ID 超出范围", "vJoy 按键 ID 超出范围");
+                        e.Handled = true;
+                        return;
+                    }
+                }
+                catch
+                {
+                    _messageBoxService.ShowMessage("vJoy 按键 ID 非法", "vJoy 按键 ID 非法");
+                    e.Handled = true;
+                    return;
+                }
+            }
+            else if (textBox.Name == "PauseHotKeyTextBox") _currKeyID = 129;
+            else if (textBox.Name == "SettingHotKeyTextBox") _currKeyID = 130;
+            else if (textBox.Name == "ResetJoystickHotKeyTextBox") _currKeyID = 131;
+            else
+            {
+                _currKeyID = 0;
+                e.Handled = true;
+                return;
+            }
+
+            try
+            {
+                //判断Key是否合法
+                if (IsForbiddenKey(e.Key))
+                {
+                    // 如果按键非法则阻止按键事件继续传播
+                    e.Handled = true;
+                    return;
+                }
+                //将用户输入的键位赋值到变量中
+                _currHotKey = e.Key;
+                _currHotKeyModifiers = Keyboard.Modifiers;
+                textBox.Text = $"{_currHotKeyModifiers} + {_currHotKey}";
+                _pressedKeys.Add(_currHotKey);
+            }
+            catch (Exception ex)
+            {
+                _messageBoxService.ShowMessage(ex.ToString(), "Error!");
+                return;
+            }
+        }
+
+        //非法Key枚举
+        private bool IsForbiddenKey(Key key)
+        {
+            return Enum.IsDefined(typeof(ForbiddenKeys), key.ToString());
+        }
+        private enum ForbiddenKeys
+        {
+            Capital, CapsLock,
+            Enter,
+            LeftAlt, LeftCtrl, LeftShift, LWin,
+            ImeProcessed,
+            NumLock, None,
+            RightAlt, RightCtrl, RightShift, RWin,
+            System,
+            Tab,
+            VolumeDown, VolumeMute, VolumeUp
+        };
+
+        private void HotKeyTextBoxPreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            //判断Key是否合法
+            if (IsForbiddenKey(e.Key))
+            {
+                // 如果按键非法则阻止按键事件继续传播
+                e.Handled = true;
+                return;
+            }
+            _pressedKeys.Remove(e.Key);
+            Console.WriteLine(_pressedKeys.Count);
+            if (_pressedKeys.Count == 0)
+            {
+                _hotKeyArray[_currKeyID] = _currHotKey;
+                _modifierKeyArray[_currKeyID] = _currHotKeyModifiers;
+                Console.WriteLine($"{_currHotKeyModifiers} + {_currHotKey}");
+
+            }
+            e.Handled = true;
+        }
 
         #endregion
 
-        #region Y轴
-        #endregion
 
         //读取设置并恢复对应UI显示文本
         private void ReadSettings()
@@ -132,8 +218,6 @@ namespace MouseSteeringWheel.Views
 
         private void SaveButtonClicked(object sender, RoutedEventArgs e)
         {
-            var messageBoxService = new MessageBoxService();
-
             // 保存通用设置，需重启后生效！！！
             // 保存设备ID
             try
@@ -143,19 +227,19 @@ namespace MouseSteeringWheel.Views
                 {
                     if (newvJoyDeviceID > 16 || newvJoyDeviceID <= 0)
                     {
-                        messageBoxService.ShowMessage("vJoy 设备 ID 超出范围", "保存失败");
+                        _messageBoxService.ShowMessage("vJoy 设备 ID 超出范围", "保存失败");
                         return;
                     }
                     else
                     {
-                        messageBoxService.ShowMessage("vJoy 设备 ID 已修改，请重启程序以使更改生效", "程序需要重启");
+                        _messageBoxService.ShowMessage("vJoy 设备 ID 已修改，请重启程序以使更改生效", "程序需要重启");
                         Settings.Default.vJoyDeviceId = newvJoyDeviceID;
                     }
                 }
             }
             catch
             {
-                messageBoxService.ShowMessage("vJoy 设备 ID 数值无效", "vJoy 设备 ID 数值无效");
+                _messageBoxService.ShowMessage("vJoy 设备 ID 数值无效", "vJoy 设备 ID 数值无效");
                 return;
             }
 
@@ -189,7 +273,7 @@ namespace MouseSteeringWheel.Views
             }
             catch
             {
-                messageBoxService.ShowMessage("UI 缩放数值无效", "UI 缩放数值无效");
+                _messageBoxService.ShowMessage("UI 缩放数值无效", "UI 缩放数值无效");
                 return;
             }
 
@@ -208,7 +292,7 @@ namespace MouseSteeringWheel.Views
             }
             catch
             {
-                messageBoxService.ShowMessage("UI Y轴偏移像素数值无效", "UI Y轴偏移像素数值无效");
+                _messageBoxService.ShowMessage("UI Y轴偏移像素数值无效", "UI Y轴偏移像素数值无效");
                 return;
             }
 
@@ -231,8 +315,13 @@ namespace MouseSteeringWheel.Views
             }
             catch
             {
-                messageBoxService.ShowMessage("XY轴数值无效", "XY轴数值无效");
+                _messageBoxService.ShowMessage("XY轴数值无效", "XY轴数值无效");
                 return;
+            }
+
+            for (int i = 1; i < 132; i++)
+            {
+                Console.WriteLine($"{i},{_hotKeyArray[i]},{_modifierKeyArray[i]}");
             }
 
 
