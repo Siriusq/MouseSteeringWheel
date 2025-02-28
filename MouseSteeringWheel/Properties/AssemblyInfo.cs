15f526525a169af95faa7a61799ea95019641866
Siriusq <eargon.skyle@gmail.com>
2025-02-28 23:19:11 +0800

    Add Icon

--- MouseSteeringWheel/Properties/AssemblyInfo.cs
+++ MouseSteeringWheel/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("MouseSteeringWheel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("siriusq.top")]
 [assembly: AssemblyProduct("MouseSteeringWheel")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
